5b129d07f7cfa29f7936f0b8a9ef1140ad5fa411
Andreas Rappold <arappold@gmx.at>
2015-05-29 12:07:52 +0200

    license & description update

--- R/docopulae.R
+++ R/docopulae.R
@@ -1,11 +1,6 @@
 #' Design of Experiments with Copulas
 #' 
-#' docopulae offers a set of functions to define parametric models by their
-#' joint density (likelihood), evaluate the expected Fisher information,
-#' find D- and Ds-optimal designs and eventually compute common statistics.
-#' A crucial aspect is modularity, so the user may replace any function
-#' by a specialized solution. The package also consists of a number of
-#' convenience functions for general purpose and specific to the task.
+#' docopulae offers a set of functions to define parametric models by their joint density (likelihood), evaluate the Fisher information, find D- and Ds-optimal designs and eventually compute common statistics. A crucial aspect is modularity, so the user may replace any function by a specialized solution. The package also consists of a number of convenience functions for general purpose and specific to the task.
 #' 
 #' TODO
 #' 
